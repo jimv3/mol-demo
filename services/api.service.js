bf4529746476484dc69c302e73267bc6e9383003
Jim Vaughan <jvaughan3@gmail.com>
2023-07-15 16:30:46 -0500

    Convert to proper class
    Add heartbeat function

--- services/api.service.js
+++ services/api.service.js
@@ -1,13 +1,33 @@
+"use strict"
 const ApiService = require("moleculer-web");
-module.exports = {
-    name: "api",
-    mixins: [ApiService],
-    settings: {
-        port: 7213,
-        routes: [{
-            path: "/api",
-            aliases: {},
-            whitelist: "**"
-        }]
+const Service = require("moleculer").Service;
+
+class MyApi extends Service {
+    constructor(broker) {
+        super(broker);
+        this.parseServiceSchema({
+            name: "api",
+            mixins: [ApiService],
+            settings: {
+                port: 7213,
+                routes: [{
+                    path: "/api",
+                    aliases: {
+                        "GET heartbeat": "api.heartbeat"
+                    },
+                    whitelist: "**"
+                }]
+            },
+            actions: {
+                heartbeat(ctx) {
+                    return ctx.nodeID;
+                }
+            },
+            async started() {
+                this.logger.info("MyApi was succesfully started");
+            }
+        });
     }
 }
+
+module.exports = MyApi;
